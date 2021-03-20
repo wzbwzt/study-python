c559d28bee460bc58dd725f0151ca84b8decc529
root <root@localhost.localdomain>
2021-03-20 06:10:51 -0400

    first commit

---
+++ 1.py
@@ -0,0 +1,15 @@
+
+how_many_snakes = 3
+snake_string = """
+Welcome to Python3!
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+<3, Juno
+"""
+ 
+ 
+print(snake_string * how_many_snakes)
