3647dddf23436cabddd5d41e5cd2304e684102ed
root <root@localhost.localdomain>
2021-03-25 22:57:15 -0400

    update

---
+++ set.py
@@ -0,0 +1,254 @@
+#集合
+'''
+set 与 dict 类似，也是一组 key 的集合，但不存储 value 。由于 key 不能重复，所以，在 set 中，没有重复
+的 key
+集合的两个特点：无序 (unordered) 和唯一 (unique)。
+
+注意， key 为不可变类型，即可哈希的值。
+'''
+num = {}
+print(type(num)) # <class 'dict'>
+num = {1, 2, 3, 4}
+print(type(num)) # <class 'set'>
+
+#集合的创建
+'''
+1. 先创建对象再加入元素。
+2. 在创建空集合的时候只能使用 s = set() ，因为 s = {} 创建的是空字典。
+'''
+basket = set()
+basket.add('apple')
+basket.add('banana')
+print(basket) # {'banana', 'apple'}
+
+'''
+1. 直接把一堆元素用花括号括起来 {元素1, 元素2, ..., 元素n} 。
+2. 重复元素在 set 中会被自动被过滤。
+'''
+basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
+print(basket) # {'banana', 'apple', 'pear', 'orange'}
+
+'''
+使用 set(value) 工厂函数，把列表或元组转换成集合。
+'''
+a = set('abracadabra')
+print(a) 
+# {'r', 'b', 'd', 'c', 'a'}
+b = set(("Google", "Lsgogroup", "Taobao", "Taobao"))
+print(b) 
+# {'Taobao', 'Lsgogroup', 'Google'}
+c = set(["Google", "Lsgogroup", "Taobao", "Google"])
+print(c) 
+# {'Taobao', 'Lsgogroup', 'Google'}
+
+#访问集合中的值
+'''
+使用 len() 內建函数得到集合的大小
+'''
+thisset = set(['Google', 'Baidu', 'Taobao'])
+print(len(thisset)) # 3
+
+'''
+可以使用 for 把集合中的数据一个个读取出来。
+'''
+thisset = set(['Google', 'Baidu', 'Taobao'])
+for item in thisset:
+ print(item)
+# Baidu
+# Google
+# Taobao
+'''
+通过 in 或 not in 判断一个元素是否在集合中已经存在
+'''
+thisset = set(['Google', 'Baidu', 'Taobao'])
+print('Taobao' in thisset) # True
+print('Facebook' not in thisset) # True
+
+#集合的内置方法
+'''
+set.add(elmnt) 用于给集合添加元素，如果添加的元素在集合中已存在，则不执行任何操作
+'''
+fruits = {"apple", "banana", "cherry"}
+fruits.add("orange")
+print(fruits) 
+# {'orange', 'cherry', 'banana', 'apple'}
+fruits.add("apple")
+print(fruits) 
+# {'orange', 'cherry', 'banana', 'apple'}
+
+'''
+set.update(set) 用于修改当前集合，可以添加新的元素或集合到当前集合中，如果添加的元素在集合中已存在，
+则该元素只会出现一次，重复的会忽略。
+'''
+x = {"apple", "banana", "cherry"}
+y = {"google", "baidu", "apple"}
+x.update(y)
+print(x)
+# {'cherry', 'banana', 'apple', 'google', 'baidu'}
+y.update(["lsgo", "dreamtech"])
+print(y)
+# {'lsgo', 'baidu', 'dreamtech', 'apple', 'google'}
+
+'''
+set.remove(item) 用于移除集合中的指定元素。如果元素不存在，则会发生错误。
+set.discard(value) 用于移除指定的集合元素。 remove() 方法在移除一个不存在的元素时会发生错误，而
+discard() 方法不会。
+'''
+fruits = {"apple", "banana", "cherry"}
+fruits.remove("banana")
+print(fruits) # {'apple', 'cherry'}
+
+'''
+set.pop() 用于随机移除一个元素
+'''
+fruits = {"apple", "banana", "cherry"}
+x = fruits.pop()
+print(fruits) # {'cherry', 'apple'}
+print(x) # banana
+
+'''
+1.set.intersection(set1, set2 ...) 返回两个集合的交集。
+2. set1 & set2 返回两个集合的交集。
+3. set.intersection_update(set1, set2 ...) 交集，在原始的集合上移除不重叠的元素。
+'''
+a = set('abracadabra')
+b = set('alacazam')
+print(a) # {'r', 'a', 'c', 'b', 'd'}
+print(b) # {'c', 'a', 'l', 'm', 'z'}
+c = a.intersection(b)
+print(c) # {'a', 'c'}
+print(a & b) # {'c', 'a'}
+print(a) # {'a', 'r', 'c', 'b', 'd'}
+a.intersection_update(b)
+print(a) # {'a', 'c'}
+
+'''
+1.set.union(set1, set2...) 返回两个集合的并集。
+2. set1 | set2 返回两个集合的并集。
+'''
+a = set('abracadabra')
+b = set('alacazam')
+print(a) # {'r', 'a', 'c', 'b', 'd'}
+print(b) # {'c', 'a', 'l', 'm', 'z'}
+print(a | b) # {'l', 'd', 'm', 'b', 'a', 'r', 'z', 'c'}
+c = a.union(b)
+print(c) # {'c', 'a', 'd', 'm', 'r', 'b', 'z', 'l'}
+
+'''
+1.set.difference(set) 返回集合的差集。
+2. set1 - set2 返回集合的差集。
+3. set.difference_update(set) 集合的差集，直接在原来的集合中移除元素，没有返回值
+'''
+a = set('abracadabra')
+b = set('alacazam')
+print(a) # {'r', 'a', 'c', 'b', 'd'}
+print(b) # {'c', 'a', 'l', 'm', 'z'}
+c = a.difference(b)
+print(c) # {'b', 'd', 'r'}
+print(a - b) # {'d', 'b', 'r'}
+print(a) # {'r', 'd', 'c', 'a', 'b'}
+a.difference_update(b)
+print(a) # {'d', 'r', 'b'}
+
+'''
+1.set.symmetric_difference(set) 返回集合的异或。
+2. set1 ^ set2 返回集合的异或。
+3. set.symmetric_difference_update(set) 移除当前集合中在另外一个指定集合相同的元素，并将另外一个指定
+集合中不同的元素插入到当前集合中
+'''
+a = set('abracadabra')
+b = set('alacazam')
+print(a) # {'r', 'a', 'c', 'b', 'd'}
+print(b) # {'c', 'a', 'l', 'm', 'z'}
+c = a.symmetric_difference(b)
+print(c) # {'m', 'r', 'l', 'b', 'z', 'd'}
+print(a ^ b) # {'m', 'r', 'l', 'b', 'z', 'd'}
+print(a) # {'r', 'd', 'c', 'a', 'b'}
+a.symmetric_difference_update(b)
+print(a) # {'r', 'b', 'm', 'l', 'z', 'd'}
+
+'''
+1.set.issubset(set) 判断集合是不是被其他集合包含，如果是则返回 True，否则返回 False。
+2. set1 <= set2 判断集合是不是被其他集合包含，如果是则返回 True，否则返回 False。
+'''
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b", "a"}
+z = x.issubset(y)
+print(z) # True
+print(x <= y) # True
+x = {"a", "b", "c"}
+y = {"f", "e", "d", "c", "b"}
+z = x.issubset(y)
+print(z) # False
+print(x <= y) # False
+
+'''
+1. set.issuperset(set) 用于判断集合是不是包含其他集合，如果是则返回 True，否则返回 False。
+2. set1 >= set2 判断集合是不是包含其他集合，如果是则返回 True，否则返回 False。
+'''
+x = {"f", "e", "d", "c", "b", "a"}
+y = {"a", "b", "c"}
+z = x.issuperset(y)
+print(z) # True
+print(x >= y) # True
+x = {"f", "e", "d", "c", "b"}
+y = {"a", "b", "c"}
+z = x.issuperset(y)
+print(z) # False
+print(x >= y) # False
+
+'''
+set.isdisjoint(set) 用于判断两个集合是不是不相交，如果是返回 True，否则返回 False。
+'''
+x = {"f", "e", "d", "c", "b"}
+y = {"a", "b", "c"}
+z = x.isdisjoint(y)
+print(z) # False
+x = {"f", "e", "d", "m", "g"}
+y = {"a", "b", "c"}
+z = x.isdisjoint(y)
+print(z) # True
+
+'''
+集合的转换
+'''
+se = set(range(4))
+li = list(se)
+tu = tuple(se)
+print(se, type(se)) # {0, 1, 2, 3} <class 'set'>
+print(li, type(li)) # [0, 1, 2, 3] <class 'list'>
+print(tu, type(tu)) # (0, 1, 2, 3) <class 'tuple'>
+
+'''
+不可变集合
+Python 提供了不能改变元素的集合的实现版本，即不能增加或删除元素，类型名叫 frozenset 。需要注意的
+是 frozenset 仍然可以进行集合操作，只是不能用带有 update 的方法。
+'''
+
+a = frozenset(range(10)) # 生成一个新的不可变集合
+print(a) 
+# frozenset({0, 1, 2, 3, 4, 5, 6, 7, 8, 9})
+b = frozenset('lsgogroup')
+print(b) 
+# frozenset({'g', 's', 'p', 'r', 'u', 'o', 'l'})
+
+'''
+zip(iter1 [,iter2 [...]])
+
+a. 用于将可迭代的对象作为参数，将对象中对应的元素打包成一个个元组，然后返回由这些元组组成的对象，这样
+做的好处是节约了不少的内存。
+b. 我们可以使用 list() 转换来输出列表。
+c. 如果各个迭代器的元素个数不一致，则返回列表长度与最短的对象相同，利用 * 号操作符，可以将元组解压为
+列表。
+'''
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [4, 5, 6, 7, 8]
+zipped = zip(a, b)
+print(zipped) # <zip object at 0x000000C5D89EDD88>
+print(list(zipped)) # [(1, 4), (2, 5), (3, 6)]
+zipped = zip(a, c)
+print(list(zipped)) # [(1, 4), (2, 5), (3, 6)]
+a1, a2 = zip(*zip(a, b))
+print(list(a1)) # [1, 2, 3]
+print(list(a2)) # [4, 5, 6]
---
+++ 流程控制.py
@@ -0,0 +1,225 @@
+# 条件语句
+'''
+if expression:
+    expr_true_suite
+elif expression2:
+    expr2_true_suite
+else
+    expr_false_suite
+
+1.使用缩进而不是大括号来标记代码块边界，因此要特别注意 else 的悬挂问题。
+2.单个 if 语句中的 expression 条件表达式可以通过布尔操作符 and ， or 和 not 实现多重条件判断。
+e.g.
+'''
+if 2 > 1 and not 2 > 3:
+    print('Correct Judgement!')
+# Correct Judgement!
+
+# assert
+'''
+assert 这个关键词我们称之为“断言”，当这个关键词后边的条件为 False 时，程序自动崩溃并抛
+出 AssertionError 的异常。
+'''
+my_list = ['lsgogroup']
+my_list.pop(0)
+assert len(my_list) > 0
+# AssertionError
+
+# while
+'''
+while 布尔表达式:
+    代码块
+
+1.while 语句最基本的形式包括一个位于顶部的布尔表达式，一个或多个属于 while 代码块的缩进语句
+2.while 循环的代码块会一直循环执行，直到布尔表达式的值为布尔假
+3.如果布尔表达式不带有 <、>、==、！=、in、not in 等运算符，仅仅给出数值之类的条件，也是可以的。当 while 后写
+入一个非零整数时，视为真值，执行循环体；写入 0 时，视为假值，不执行循环体。也可以写入 str、list 或任何序
+列，长度非零则视为真值，执行循环体；否则视为假值，不执行循环体。
+'''
+
+'''
+while 布尔表达式:
+    代码块
+else:
+    代码块
+当 while 循环正常执行完的情况下，执行 else 输出，如果 while 循环中执行了跳出循环的语句，比如 break ，将不执
+行 else 代码块的内容
+'''
+
+# for
+'''
+for 迭代变量 in 可迭代对象:
+    代码块
+
+1.for 循环是迭代循环，在Python中相当于一个通用的序列迭代器，可以遍历任何有序序列，如 str、list、tuple 等，也
+可以遍历任何可迭代对象，如 dict 。
+- for i in 'ILoveLSGO':
+
+- member = ['张三', '李四', '刘德华', '刘六', '周润发']
+    for each in member:
+
+- for i in range(len(member)):
+
+- dic = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+for key, value in dic.items():
+for key in dic.keys():
+for key in dic.values():
+'''
+
+'''
+for 迭代变量 in 可迭代对象:
+    代码块
+else:
+    代码块
+
+1.当 for 循环正常执行完的情况下，执行 else 输出，如果 for 循环中执行了跳出循环的语句，比如 break ，将不执
+行 else 代码块的内容，与 while - else 语句一样
+'''
+
+# range
+'''
+range([start,] stop[, step=1])
+
+1.这个BIF（Built-in functions）有三个参数，其中用中括号括起来的两个表示这两个参数是可选的。
+2. step=1 表示第三个参数的默认值是1。
+3. range 这个BIF的作用是生成一个从 start 参数的值开始到 stop 参数的值结束的数字序列，该序列包含 start 的
+值但不包含 stop 的值。
+
+'''
+for i in range(2, 9):  # 不包含9
+    print(i)
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+
+for i in range(1, 10, 2):
+    print(i)
+# 1
+# 3
+# 5
+# 7
+# 9
+
+# enumerate
+'''
+enumerate(sequence, [start=0])
+
+1. sequence -- 一个序列、迭代器或其他支持迭代对象。
+2. start -- 下标起始位置。
+3. 返回 enumerate(枚举) 对象
+
+
+'''
+seasons = ['Spring', 'Summer', 'Fall', 'Winter']
+lst = list(enumerate(seasons))
+print(lst)
+# [(0, 'Spring'), (1, 'Summer'), (2, 'Fall'), (3, 'Winter')]
+lst = list(enumerate(seasons, start=1))  # 下标从 1 开始
+print(lst)
+# [(1, 'Spring'), (2, 'Summer'), (3, 'Fall'), (4, 'Winter')]
+
+# enumerate() 与 for 循环的结合使用
+'''
+for i, a in enumerate(A)
+ do something with a
+
+用 enumerate(A) 不仅返回了 A 中的元素，还顺便给该元素一个索引值 (默认从 0 开始)。此外，用
+enumerate(A, j) 还可以确定索引起始值为 j
+'''
+
+# 流程终止
+'''
+1.break 语句可以跳出当前所在层的循环。
+2.continue 终止本轮循环并开始下一轮循环。
+3.pass 语句的意思是“不做任何事”，如果你在需要有语句的地方不写任何语句，那么解释器会提示出错，而 pass 语句就
+是用来解决这些问题的。
+
+'''
+# 推导式
+'''
+[ expr for value in collection [if condition] ]
+
+'''
+
+# 列表推导式
+x = [-4, -2, 0, 2, 4]
+y = [a * 2 for a in x]
+print(y)
+# [-8, -4, 0, 4, 8]
+
+x = [i ** 2 for i in range(1, 10)]
+print(x)
+# [1, 4, 9, 16, 25, 36, 49, 64, 81]
+
+x = [(i, i ** 2) for i in range(6)]
+print(x)
+# [(0, 0), (1, 1), (2, 4), (3, 9), (4, 16), (5, 25)]
+
+x = [i for i in range(100) if (i % 2) != 0 and (i % 3) == 0]
+print(x)
+# [3, 9, 15, 21, 27, 33, 39, 45, 51, 57, 63, 69, 75, 81, 87, 93, 99]
+
+a = [(i, j) for i in range(0, 3) for j in range(0, 3)]
+print(a)
+# [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1), (1, 2), (2, 0), (2, 1), (2, 2)]
+
+x = [[i, j] for i in range(0, 3) for j in range(0, 3)]
+print(x)
+# [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+x[0][0] = 10
+print(x)
+# [[10, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+
+a = [(i, j) for i in range(0, 3) if i < 1 for j in range(0, 3) if j > 1]
+print(a)
+# [(0, 2)]
+
+# 元组推导式
+'''
+( expr for value in collection [if condition] )
+
+'''
+a = (x for x in range(10))
+print(a)
+# <generator object <genexpr> at 0x0000025BE511CC48>
+print(tuple(a))
+# (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+# 字典推导式
+'''
+{ key_expr: value_expr for value in collection [if condition] }
+
+'''
+b = {i: i % 2 == 0 for i in range(10) if i % 3 == 0}
+print(b)
+# {0: True, 3: False, 6: True, 9: False}
+
+# 集合推导式
+'''
+{ expr for value in collection [if condition] }
+
+'''
+c = {i for i in [1, 2, 3, 4, 5, 5, 6, 4, 3, 2, 1]}
+print(c)
+# {1, 2, 3, 4, 5, 6}
+
+# 其它
+d = 'i for i in "I Love Lsgogroup"'
+print(d)
+# i for i in "I Love Lsgogroup"
+e = (i for i in range(10))
+print(e)
+# <generator object <genexpr> at 0x0000007A0B8D01B0>
+print(next(e))  # 0
+print(next(e))  # 1
+for each in e:
+    print(each, end=' ')
+# 2 3 4 5 6 7 8 9
+s = sum([i for i in range(101)])
+print(s)  # 5050
+s = sum((i for i in range(101)))
+print(s)  # 5050
